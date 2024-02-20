875f46e198026cf26c056aa27a344a003c005368
Ameresh Mishra <73485115+amresh012@users.noreply.github.com>
2024-02-20 20:54:14 +0530

    Update Row.js

--- src/component/Row.js
+++ src/component/Row.js
@@ -16,7 +16,7 @@ export const Row = ({ title, fetchURL }) => {
         <div className='relative flex items-center'>
             <div id={`slider`}>
                 {movies.map((item ,id )=>{
-                    <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer'>
+                  return  <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer'>
                         <img src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item.title} />
                     </div>
                 })}
